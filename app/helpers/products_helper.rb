563c0c2be781475bb70517c72170b5817f710da4
dan rey oquindo <danreyoquindo@gmail.com>
2016-11-08 16:06:43 +0800

    Fixes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,10 @@
 module ProductsHelper
+
+  def active_category_menu?(category = nil)
+    if defined?(@product)
+      @product.category == category
+    elsif params[:name].present?
+      params[:name] == category.name
+    end
+  end
 end
